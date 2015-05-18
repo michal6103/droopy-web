15fcbcc3489a1e7026bbf9719f94f625e9547eb3
Michal Sedlak <michal6103@gmail.com>
2015-05-18 12:46:44 -0400

    Basic tests for is_colinear and orientation

--- droopy_web_test.py
+++ droopy_web_test.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 import unittest
 # from droopy_web import app
-from droopy_web import is_colinear
+from droopy_web import is_colinear, orientation
 
 
 class FlaskTestCase(unittest.TestCase):
@@ -10,15 +11,31 @@ class FlaskTestCase(unittest.TestCase):
     non_colinear_points = [((0, 0), (1, 1), (2, 2), (2, 3)),
                            ((0, 0), (1, 1), (0, 0), (1, 3))]
 
+    ccw_points = [((0, 0), (1, 0), (1, 1)),
+                  ((0, 0), (0, 1), (-1, 0)),
+                  ((0, 0), (-1, 0), (0, -1))]
+
+    cw_points = [((0, 0), (1, 0), (1, -1)),
+                 ((0, 0), (0, 1), (1, 1)),
+                 ((0, 0), (-1, 0), (-1, 1))]
+
     def test_is_colinear(self):
         for a, b, c, d in self.colinear_points:
-            print("a: {}\t b:{}\tc:{}\td:{}".format(a, b, c, d))
             self.assertTrue(is_colinear((a, b), (c, d)))
 
         for a, b, c, d in self.non_colinear_points:
-            print("a: {}\t b:{}\tc:{}\td:{}".format(a, b, c, d))
             self.assertFalse(is_colinear((a, b), (c, d)))
 
+    def test_orientation(self):
+        for a, b, c in self.cw_points:
+            self.assertEqual(orientation(a, b, c), -1)
+        for a, b, c in self.ccw_points:
+            self.assertEqual(orientation(a, b, c), 1)
+        for a, b, c, d in self.colinear_points:
+            self.assertEqual(orientation(a, b, c), 0)
+
+    def test_bounding_boxes(self):
+        
 
 if __name__ == '__main__':
     unittest.main()
