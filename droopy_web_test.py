1f4060150aa912a71fba358e90659148d731adb3
Michal Sedlak <michal6103@gmail.com>
2015-06-29 12:12:05 -0400

    small testcase for intersectino and untangling

--- droopy_web_test.py
+++ droopy_web_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import unittest
 # from droopy_web import app
-from droopy_web import is_colinear, orientation
+from droopy_web import is_colinear, orientation, untangle
 
 
 class FlaskTestCase(unittest.TestCase):
@@ -13,11 +13,15 @@ class FlaskTestCase(unittest.TestCase):
 
     ccw_points = [((0, 0), (1, 0), (1, 1)),
                   ((0, 0), (0, 1), (-1, 0)),
-                  ((0, 0), (-1, 0), (0, -1))]
+                  ((0, 0), (-1, 0), (0, -1)),
+                  ((91, 51), (89, 47), (90, 48))]
 
     cw_points = [((0, 0), (1, 0), (1, -1)),
                  ((0, 0), (0, 1), (1, 1)),
                  ((0, 0), (-1, 0), (-1, 1))]
+
+    untangle_path = [(0, 0), (0, 0), (1, 1), (2, 2), (1, 3), (0, 3), (4, 0)]
+    untangled_path = [(0, 0), (0, 0), (1, 1), (0, 3), (2, 2), (1, 3), (4, 0)]
 
     def test_is_colinear(self):
         for a, b, c, d in self.colinear_points:
@@ -35,7 +39,10 @@ class FlaskTestCase(unittest.TestCase):
             self.assertEqual(orientation(a, b, c), 0)
 
     def test_bounding_boxes(self):
-        
+        pass
+
+    def test_untabngle_path(self):
+        self.assertEqual(untangle(self.untangle_path), self.untangled_path)
 
 if __name__ == '__main__':
     unittest.main()
